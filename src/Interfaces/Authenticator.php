d01b11c8c723ebba8f77ddd771338336630e4a24
Deusagnis <deusagnis@gmail.com>
2023-03-08 18:28:37 +0300

    Fix throws doc.

--- src/Interfaces/Authenticator.php
+++ src/Interfaces/Authenticator.php
@@ -2,12 +2,15 @@
 
 namespace MGGFLOW\AuthBase\Interfaces;
 
+use MGGFLOW\AuthBase\Exceptions\AuthBaseException;
+
 interface Authenticator
 {
     /**
      * Authenticate by looking up User data.
      *
      * @return Authenticator
+     * @throws AuthBaseException
      */
     public function auth(): self;
 }
